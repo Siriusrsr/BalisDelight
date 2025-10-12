0bf5a289ecd8e355844e833eda7fe3d3c92154b6
Siriusrsr <x_yang2004@hotmail.com>
2025-10-12 12:57:56 +0800

    增加测试方块

---
+++ src/main/java/org/bali/balisdelight/common/Block/TestBlock.java
@@ -0,0 +1,9 @@
+package org.bali.balisdelight.common.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class TestBlock extends Block {
+    public TestBlock(Properties properties){
+        super(properties);
+    }
+}
